cf06add002d5cbc3f084b82c46f1c8ce17cdab1c
Bas De Bue <bas.de.bue@edpnet.be>
2019-06-11 21:04:23 +0200

    Refactor `client` module

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,9 @@
 //! A client for interacting with a Smartschool instance.
 
 use crate::error::{Error, Result};
-use futures::compat::Future01CompatExt;
-use futures_01::stream::Stream;
+use futures::compat::{Future01CompatExt, Stream01CompatExt};
+use futures::stream::TryStreamExt;
 use regex::Regex;
-use reqwest::header::{HeaderMap, COOKIE, SET_COOKIE};
 use reqwest::r#async::Client as HttpClient;
 use reqwest::RedirectPolicy;
 use std::collections::HashMap;
@@ -13,7 +12,6 @@ use std::collections::HashMap;
 #[derive(Clone, Debug)]
 pub struct Client<'a> {
     http_client: HttpClient,
-    session_id: String,
     url: &'a str,
 }
 
@@ -25,39 +23,82 @@ impl<'a> Client<'a> {
 
     /// Creates a client from login credentials.
     ///
+    /// # Errors
+    ///
+    /// This function returns an error if the server responds with an invalid encoding or if the response does not contain a login token.
+    /// Note that the absence of a login token isn't necessarily the server's fault; this error also occurs if the user specifies an invalid URL or an unsupported protocol.
+    ///
     /// # Examples
     ///
     /// Basic usage:
     ///
-    /// ```rust
-    /// let client = Client::login(
-    ///     "https://myschool.smartschool.be",
-    ///     "username",
-    ///     "password"
-    /// ).await?;
+    /// ```ignore
+    /// #![feature(async_await)]
+    ///
+    /// use smartschool::Client;
+    ///
+    /// let client = Client::login("https://myschool.smartschool.be", "username", "password").await?;
+    /// assert_eq!("https://myschool.smartschool.be", client.url());
     /// ```
+    ///
+    /// # Implementation
+    ///
+    /// Logging into Smartschool follows a fairly complex process.
+    ///
+    /// [1] First, we have to make a GET request to the `/login` path.
+    /// [2] We then have to save the `PHPSESSID` cookie contained in the response headers.
+    /// [3] Next, we need to extract the login token from the HTML response's body.
+    /// [4] We're almost done - we now have to send all our authentication data to `/login`.
+    /// This means making a POST request containing username, password, token and the `PHPSESSID` cookie from earlier.
+    /// [5] Finally, we have to get a new `PHPSESSID` cookie from the response headers.
+    /// This cookie can then be used to authenticate oneself across the platform.
+    /// If the headers don't contain a new `PHPSESSID`, the credentials are invalid or the token has expired, and we return an error.
+    ///
+    /// Since cookies are managed automatically by the HTTP client's cookie store, the `PHPSESSID` cookie doesn't get explicitly mentioned in the code.
     pub async fn login(url: &'a str, username: &'a str, password: &'a str) -> Result<Client<'a>> {
         let http_client = HttpClient::builder()
+            .cookie_store(true)
             .redirect(RedirectPolicy::none())
             .build()?;
 
-        let (session_id, token) = get_session_id_and_token(&http_client, url).await?;
-        let session_id =
-            post_login_credentials(&http_client, url, &session_id, username, password, &token)
-                .await?;
+        // step 1 and step 2
+        let request_url = format!("{}/login", url);
+        let response = http_client
+            .get(&request_url)
+            .send()
+            .compat()
+            .await?
+            .error_for_status()?;
 
-        Ok(Client {
-            http_client,
-            session_id,
-            url,
-        })
-    }
+        // step 3
+        let body = response.into_body().compat().try_concat().await?;
+        let body = std::str::from_utf8(&body)
+            .map_err(|_| Error::Server("Server response was not UTF-8-encoded"))?;
+        let token =
+            get_token(body).ok_or(Error::Server("Server response did not contain login token"))?;
 
-    /// Returns the current session's PHPSESSID.
-    ///
-    /// The session ID is generated from the user's login credentials and is the primary authentication method used throughout Smartschool.
-    pub fn session_id(&self) -> &str {
-        &self.session_id
+        // step 4 and step 5
+        let mut form = HashMap::new();
+        form.insert("login_form[_password]", password);
+        form.insert("login_form[_token]", token);
+        form.insert("login_form[_username]", username);
+        let response = http_client
+            .post(&request_url)
+            .form(&form)
+            .send()
+            .compat()
+            .await?
+            .error_for_status()?;
+
+        let successful = response
+            .cookies()
+            .any(|cookie| cookie.name() == "PHPSESSID");
+
+        if successful {
+            Ok(Client { http_client, url })
+        } else {
+            Err(Error::Client("Invalid login credentials"))
+        }
     }
 
     /// Returns the URL of the associated Smartschool instance.
@@ -66,83 +107,13 @@ impl<'a> Client<'a> {
     }
 }
 
-/// Extracts the session ID and login token from the login page.
-async fn get_session_id_and_token<'a>(
-    http_client: &'a HttpClient,
-    url: &'a str,
-) -> Result<(String, String)> {
-    let url = format!("{}/login", url);
-    let response = http_client.get(&url).send().compat().await?;
-
-    let session_id = get_session_id_cookie(response.headers())
-        .ok_or(Error::Response("Server response did not contain PHPSESSID"))?
-        .to_owned();
-
-    let body = response.into_body().concat2().compat().await?;
-    let body = std::str::from_utf8(&body)
-        .map_err(|_| Error::Response("Server response was not UTF-8-encoded"))?;
-
-    let token = get_token(body)
-        .ok_or(Error::Response("Server response did not contain token"))?
-        .to_owned();
-
-    Ok((session_id, token))
-}
-
-/// Extracts a PHPSESSID cookie from response headers.
-fn get_session_id_cookie(headers: &HeaderMap) -> Option<&str> {
-    let re =
-        Regex::new("PHPSESSID=(.+?);").expect("error while creating get_session_id_cookie regex");
-    for header in headers.get_all(SET_COOKIE) {
-        let cookie = header.to_str().ok()?;
-        if let Some(captures) = re.captures(cookie) {
-            return Some(captures.get(1)?.as_str());
-        }
-    }
-    None
-}
-
 /// Extracts the login token from a response body.
 fn get_token(body: &str) -> Option<&str> {
     // The token's <input> element happens to be the only one on the page that has two spaces before the `value` attribute.
     // If you get an error saying the token is missing, this trick probably doesn't work anymore.
-    let re = Regex::new("  value=\"(.+?)\"").expect("error while creating get_token regex");
-    if let Some(captures) = re.captures(body) {
-        return Some(captures.get(1)?.as_str());
-    }
-    None
-}
-
-/// Posts login credentials in exchange for a session ID.
-async fn post_login_credentials<'a>(
-    http_client: &'a HttpClient,
-    url: &'a str,
-    session_id: &'a str,
-    username: &'a str,
-    password: &'a str,
-    token: &'a str,
-) -> Result<String> {
-    let mut form = HashMap::new();
-    form.insert("login_form[_password]", password);
-    form.insert("login_form[_token]", token);
-    form.insert("login_form[_username]", username);
-
-    let url = format!("{}/login", url);
-    let cookie = format!("PHPSESSID={}", session_id);
-    let response = http_client
-        .post(&url)
-        .header(COOKIE, cookie)
-        .form(&form)
-        .send()
-        .compat()
-        .await?;
-
-    // If the response doesn't contain a session ID, the login credentials are most likely invalid.
-    let session_id = get_session_id_cookie(response.headers())
-        .ok_or(Error::Response(
-            "Invalid login credentials or expired login token",
-        ))?
-        .to_owned();
-
-    Ok(session_id)
+    Regex::new("  value=\"(.+?)\"")
+        .expect("error while creating smartschool::client::get_token regex")
+        .captures(body)
+        .and_then(|captures| captures.get(1))
+        .map(|capture| capture.as_str())
 }
--- src/error.rs
+++ src/error.rs
@@ -3,28 +3,21 @@
 use reqwest::Error as ReqwestError;
 use std::error::Error as StdError;
 use std::fmt;
-use std::io::Error as IoError;
 
 /// An error returned by the `smartschool` crate.
 #[derive(Debug)]
 pub enum Error {
-    /// An error that occurred while performing I/O.
-    Io(IoError),
+    /// An error represening an invalid request by the client.
+    Client(&'static str),
     /// An error originating from the `reqwest` crate.
     Reqwest(ReqwestError),
     /// An error representing an unexpected response from the server.
-    Response(&'static str),
+    Server(&'static str),
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self)
-    }
-}
-
-impl From<IoError> for Error {
-    fn from(err: IoError) -> Error {
-        Error::Io(err)
     }
 }
 
@@ -36,5 +29,5 @@ impl From<ReqwestError> for Error {
 
 impl StdError for Error {}
 
-/// A specialized [`Result`](std::result::Result) type for Smartschool-related operations.
+/// A specialized [`Result`](std::result::Result) type returned by the `smartschool` crate.
 pub type Result<T> = std::result::Result<T, Error>;
