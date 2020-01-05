b6d3541af0f149f727458f664fa3afc09339041e
Bas De Bue <bas.de.bue@edpnet.be>
2020-01-05 16:35:32 +0100

    Support new login procedure
    Closes #2

--- src/client.rs
+++ src/client.rs
@@ -8,16 +8,18 @@ use regex::Regex;
 use reqwest::{redirect, Client as HttpClient};
 use std::collections::HashMap;
 
-/// Extracts the login token from a response body.
-fn get_token(body: &str) -> Option<&str> {
-    // The token's <input> element happens to be the only one on the page that has
-    // two spaces before the `value` attribute. If you get an error saying the
-    // token is missing, this trick probably doesn't work anymore.
-    Regex::new("  value=\"(.+?)\"")
-        .expect("error while compiling smartschool::client::get_token regex")
-        .captures(body)
-        .and_then(|captures| captures.get(1))
+/// Extracts the request parameters from a response body.
+fn get_params(body: &str) -> Option<(&str, &str)> {
+    // The parameters' <input> elements happen to be the only ones on the page that
+    // have two spaces before the `value` attribute. If you get a seemingly
+    // unexplainable authentication error, this trick probably stopped working.
+    let params: Vec<&str> = Regex::new("  value=\"(.+?)\"")
+        .ok()?
+        .captures_iter(body)
+        .filter_map(|captures| captures.get(1))
         .map(|capture| capture.as_str())
+        .collect();
+    Some((params.get(0)?, params.get(1)?))
 }
 
 /// An asynchronous client for interacting with a Smartschool instance.
@@ -30,24 +32,16 @@ pub struct Client<'a> {
 impl<'a> Client<'a> {
     /// Logs in with the provided login credentials and returns a client.
     ///
+    /// The URL specifies the location of the Smartschool instance, usually a
+    /// subdomain of `smartschool.be`.
+    ///
     /// # Errors
     ///
     /// Returns an error in the following situations:
     ///
+    /// * The login credentials are incorrect.
     /// * The URL is invalid or uses an unsupported protocol.
-    /// * The server response doesn't contain a login token.
-    ///
-    /// # Examples
-    ///
-    /// Basic usage:
-    ///
-    /// ```ignore
-    /// use smartschool::Client;
-    ///
-    /// let client = Client::login("https://myschool.smartschool.be", "username", "password").await?;
-    ///
-    /// assert_eq!("https://myschool.smartschool.be", client.url());
-    /// ```
+    /// * The server response didn't contain the login parameters.
     pub async fn login(url: &'a str, username: &str, password: &str) -> Result<Client<'a>> {
         let http_client = HttpClient::builder()
             .cookie_store(true)
@@ -61,9 +55,10 @@ impl<'a> Client<'a> {
             .await?
             .text()
             .await?;
-        let token = get_token(&response).ok_or(Error::Authentication)?;
+        let (gen_time, token) = get_params(&response).ok_or(Error::Authentication)?;
 
         let mut form = HashMap::new();
+        form.insert("login_form[_generationTime]", gen_time);
         form.insert("login_form[_password]", password);
         form.insert("login_form[_token]", token);
         form.insert("login_form[_username]", username);
@@ -76,7 +71,6 @@ impl<'a> Client<'a> {
         let successful = response
             .cookies()
             .any(|cookie| cookie.name() == "PHPSESSID");
-
         if successful {
             Ok(Client { http_client, url })
         } else {
